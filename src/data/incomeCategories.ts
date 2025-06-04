be78aae05516d88be67c6e042731e82b11192ab3
demqqo <138254074+demqqo@users.noreply.github.com>
2025-06-03 22:12:47 -0400

    income categories

---
+++ src/data/incomeCategories.ts
@@ -0,0 +1,18 @@
+export const incomeCategories = [
+    { "category": "Salary", "type": "regular" },
+    { "category": "Freelance", "type": "irregular" },
+    { "category": "Business", "type": "regular" },
+    { "category": "Investments", "type": "passive" },
+    { "category": "Dividends", "type": "passive" },
+    { "category": "Rental Income", "type": "passive" },
+    { "category": "Interest", "type": "passive" },
+    { "category": "Pension", "type": "regular" },
+    { "category": "Social Benefits", "type": "government" },
+    { "category": "Gifts", "type": "irregular" },
+    { "category": "Scholarship", "type": "educational" },
+    { "category": "Royalties", "type": "passive" },
+    { "category": "Grants", "type": "educational" },
+    { "category": "Selling Assets", "type": "one-time" },
+    { "category": "Side Hustle", "type": "irregular" }
+  ]
+  
