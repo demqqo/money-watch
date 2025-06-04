8dc314868bc6b81038e9a819f1af2e0a64a84362
demqqo <138254074+demqqo@users.noreply.github.com>
2025-06-03 22:07:16 -0400

    adding modal window component and css to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css'
-import Counter from './components/1Counter'
 import ExpensesList from './components/expensesList'
 
 function App() {
 
   return (
-    <>
-      <div>
-        <ExpensesList />
-      </div>
+    <div>
       
-    </>
+        <ExpensesList />
+        
+    </div>
   )
 }
 
---
+++ src/components/modal.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style/modal.scss'
+type Props = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const Modal = ({ children, isOpen, onClose }: Props) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-backdrop" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
