5fff8bee8511180e44046a5c17eed901791b2696
demqqo <138254074+demqqo@users.noreply.github.com>
2025-06-09 16:58:54 -0400

    adding authorization with google

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import './App.css'
 import ExpensesList from './components/expensesList'
+import {useEffect, useState} from 'react'
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import type {User} from 'firebase/auth'
+import { auth } from "./backend/firebase";
+import SignIn from "./components/signIn";
 
 function App() {
 
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div>
-      
-        <ExpensesList />
-        
+      {user ? (
+        <>
+          <p>Welcome, {user.displayName}</p>
+          <ExpensesList />
+          <button onClick={() => signOut(auth)}>Sign out</button>
+        </>
+      ) : (
+        <SignIn />
+      )}
     </div>
   )
 }
