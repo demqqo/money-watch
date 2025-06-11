6748639ce1414f00e40884953ad6abc8e0232f9d
demqqo <138254074+demqqo@users.noreply.github.com>
2025-06-11 14:46:14 -0400

    feat: add delete, create for SQLite for two tables incomes\expenses

--- src/backend/sqlitedb/db.js
+++ src/backend/sqlitedb/db.js
@@ -10,4 +10,21 @@ db.prepare(`
   )
     `).run();
 
+    db.prepare(`CREATE TABLE IF NOT EXISTS expenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        value REAL)`
+    ).run()
+
+    db.prepare(`CREATE TABLE IF NOT EXISTS incomes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        value REAL)`
+    ).run()
+
+    db.prepare(`DELETE FROM incomes WHERE id = ?`)
+
+    db.prepare(`DELETE FROM expenses WHERE id = ?`)
+
+
     module.exports =db
