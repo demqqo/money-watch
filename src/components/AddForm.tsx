0c957cdf98e116f23e41501a5d4ccbdf880ea1b2
demqqo <138254074+demqqo@users.noreply.github.com>
2025-06-13 18:52:01 -0400

    feat: component opened by modal window in which it determing if it is income of epense and then it renders another modal

---
+++ src/components/AddForm.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { incomeCategories } from '../data/incomeCategories';
+import {expenseCategories}  from '../data/expenseCategories';
+import ExpenseComponent from './moneyChangeRecord'
+
+import { ChangeToExpense, ChangeToIncome } from '../state/globalStates/expenseOrIncomeSlice';
+import { useAppSelector, useAppDispatch} from '../state/hooks';
+
+const AddForm = (type: any) => {
+
+    const dispatch = useAppDispatch();
+    if(type.type === 'expense'){
+        dispatch(ChangeToExpense())
+      }else{
+        dispatch(ChangeToIncome())
+      }
+    console.log(type)
+  return (
+    <div>{(type.type === 'income' ? incomeCategories : expenseCategories).map((item) => (
+        //@ts-ignore
+        <ExpenseComponent key={item.category}>{item.category}</ExpenseComponent>
+      ))}</div>
+  )
+}
+
+
+export default AddForm
