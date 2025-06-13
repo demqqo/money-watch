af9cb434b5860db0464d670112b30a1a64f947e7
demqqo <138254074+demqqo@users.noreply.github.com>
2025-06-13 18:52:55 -0400

    feat: edit form where it recieves id of object and type (expense\income) and render modal window with input to enter new value

---
+++ src/components/EditForm.tsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import Modal from './modal'
+import axios from 'axios'
+
+import { useAppSelector, useAppDispatch} from '../state/hooks';
+import {OpenModal, CloseModal, AddExpense, AddIncome} from '../state/globalStates/modalComponentSlice'
+import { ChangeToExpense, ChangeToIncome } from '../state/globalStates/expenseOrIncomeSlice';
+const EditForm = (props:any) => {
+    
+    const dispatch = useAppDispatch();
+    const open = useAppSelector((state) => state.globalState.isOpen)
+    function handleSubmit(value: number) {
+        axios.put(`http://localhost:3000/${props.categoryToChange}/${props.id}`, {
+            value: value,
+          })
+          console.log(props)
+        dispatch(CloseModal())
+       
+    }
+    
+    const [input, setInput] = useState(0)
+   
+  return (
+    <div>
+        
+        <Modal isOpen={open} onClose={()=> dispatch(CloseModal())}>
+        <h2>Enter new amount</h2>
+        <input onChange={(e) => setInput(Number(e.target.value))} placeholder="enter amount"></input>
+        <button 
+        onClick={()=>handleSubmit(input)}>
+            Confirm
+          </button>
+      </Modal>
+    </div>
+  )
+}
+
+export default EditForm
